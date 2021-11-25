ddab8f12351437d69889fc4a50a9af3c12096aa2
Ilya Gerasimov <ilya.gerasimov.97@mail.ru>
2021-11-25 23:38:26 +0300

    отображение заметки

--- app/src/main/java/com/example/notes/NoteDescriptionFragment.java
+++ app/src/main/java/com/example/notes/NoteDescriptionFragment.java
@@ -2,11 +2,16 @@ package com.example.notes;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,12 +22,11 @@ public class NoteDescriptionFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM1 = "index";
+
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int index;
 
     public NoteDescriptionFragment() {
         // Required empty public constructor
@@ -32,16 +36,14 @@ public class NoteDescriptionFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param index Parameter 1.
      * @return A new instance of fragment NoteDescriptionFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static NoteDescriptionFragment newInstance(String param1, String param2) {
+    public static NoteDescriptionFragment newInstance(int index) {
         NoteDescriptionFragment fragment = new NoteDescriptionFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_PARAM1, index);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +52,7 @@ public class NoteDescriptionFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            index = getArguments().getInt(ARG_PARAM1);
         }
     }
 
@@ -60,5 +61,18 @@ public class NoteDescriptionFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_note_description, container, false);
+    }
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String[] notes = getResources().getStringArray(R.array.notes);
+        EditText editName = view.findViewById(R.id.Name);
+        editName.setText(notes[index]);
+
+
     }
 }
--- app/src/main/java/com/example/notes/NotesFragment.java
+++ app/src/main/java/com/example/notes/NotesFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -66,11 +68,27 @@ public class NotesFragment extends Fragment {
         LinearLayout layoutView = (LinearLayout) view;
         String[] notes = getResources().getStringArray(R.array.notes);
 
-        for (String note: notes) {
+        for (int i = 0; i< notes.length; i++) {
+            String note = notes[i];
             TextView tvNoteName = new TextView(getContext());
             tvNoteName.setText(note);
             tvNoteName.setTextSize(30);
             layoutView.addView(tvNoteName);
+            final int position = i;
+            tvNoteName.setOnClickListener(v -> {
+                showNote(position);
+            });
         }
+    }
+
+    private void showNote(int position) {
+        NoteDescriptionFragment noteDescriptionFragment =
+                NoteDescriptionFragment.newInstance(position);
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.fragment_container ,noteDescriptionFragment);
+        transaction.addToBackStack("");
+        transaction.commit();
+
     }
 }
